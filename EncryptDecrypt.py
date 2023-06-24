a2b242bb0e7216c52745679832d90447b93fa73c
Anele <ngcobo.anele@gmail.com>
2023-06-24 11:45:06 +0200

    Created the encrypt code successfully

--- EncryptDecrypt.py
+++ EncryptDecrypt.py
@@ -1,1 +1,17 @@
 #Create an encryption and decryption communication code
+
+mylist= ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
+
+
+def pwd_encrypt(UserText):
+    store=""
+    for x in UserText:
+            initial_pos=mylist.index(x)
+            encrypt_pos=initial_pos+7
+            new_char=mylist[encrypt_pos]
+            store=store+new_char
+    print(store)
+
+UserText=input("Please enter message:")
+pwd_encrypt(UserText)
+        
